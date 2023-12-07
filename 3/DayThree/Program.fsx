6603ec3b53aa9b91249c40d2a93510edf559b021
axel-nils <axelnils@student.chalmers.se>
2023-12-07 16:30:41 +0100

    3 2 complete

--- 3/DayThree/Program.fsx
+++ 3/DayThree/Program.fsx
@@ -31,27 +31,40 @@ let rec extendRight (r: row) (i: int): int =
     | x when charIsNum r[x+1] -> extendRight r (x+1)
     | x -> x
     
-let extend (r: row) (i: int) =
-    (extendLeft r i, extendRight r i)
+let extend (s: schematic) (r: int) (i: int) =
+    (r, extendLeft s[r] i, extendRight s[r] i)
 
 let surroundings (s: schematic) (i: int) (a, b) =
     List.allPairs  [for x in [(if i > 0 then i-1 else i)..(if i < (s.Length-1) then i+1 else i)] -> x] [for x in [(if a > 0 then a-1 else a)..(if b < (s[i].Length-1) then b+1 else b)] -> x]
 
 let checkSurronding (s: schematic) ls =
-    [for l in ls -> s[fst l][snd l]] 
-    |> List.fold (fun acc elem -> charIsSymbol elem || acc) false 
 
-let sumNum (r: row) (a: int, b: int) = 
-    List.map (fun (n, p) -> (10.0 ** p) * n ) [for x in [a..b] -> (float(string(r[x])), float(b-x))]
+    [for l in ls -> s[fst l][snd l]] 
+    |> List.fold (fun acc elem -> (if charIsNum elem then 1 else 0) + acc) 0
+    |> (fun x -> x = 2)
+
+let sumNum (s: schematic) (r: int, a: int, b: int) = 
+    List.map (fun (n, p) -> (10.0 ** p) * n ) [for x in [a..b] -> (float(string(s[r][x])), float(b-x))]
     |> List.sum |> int
 
-let sumRow (s: schematic) (r: int) =
+let sumChar s x: int =
+    List.map (sumNum s) x 
+    |> List.fold (fun x y -> x * y) 1
+
+let getSurroundingNums s r x = 
+    surroundings s r (x, x) 
+    |> List.filter (fun (r, x) -> charIsNum (s[r][x]))
+    |> List.map (fun (a, b) -> (extend s a b))
+    |> List.distinct
+    |> fun x ->
+        match List.length x with
+        | 2 -> sumChar s x
+        | _ -> 0
+
+let sumRow (s: schematic) (r: int)=
     [0..s[r].Length-1] 
-    |> List.filter (fun x -> charIsNum (s[r][x])) 
-    |> List.map (extend (s[r])) 
-    |> List.distinct 
-    |> List.filter (fun x ->(surroundings s r x |> checkSurronding s))
-    |> List.map (sumNum s[r])
+    |> List.filter (fun x -> charIsSymbol (s[r][x])) 
+    |> List.map (getSurroundingNums s r)
     |> List.sum
 
 let sumSchematic (s: schematic) =
